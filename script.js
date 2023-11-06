ec3461459380a091a3c6678ce4bdbc177f473b90
erjia <erjia.wang.2022@scis.smu.edu.sg>
2023-11-06 15:50:01 +0800

    updated with new nav bar and loading skeleton

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // Replace with your GPT-3.5 API key
-const apiKey = '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';
+const apiKey = '';
 
 document.getElementById('generateButton').addEventListener('click', generateItinerary);
 
 function generateItinerary() {
-    const outputElement = document.getElementById('output');
     const budget = document.getElementById('budget').value;
     const location = document.getElementById('location').value;
     const days = document.getElementById('days').value;
-    const prompt = `Generate a sustainable travel itinerary for a ${days}-day trip to ${location} with a budget of $${budget}.`;
+    const outputElement = document.getElementById('output');
+    const loadingSkeleton = document.getElementById('loadingSkeleton');
 
-    axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions',  {
+    // Show the loading skeleton while generating the itinerary
+    loadingSkeleton.style.display = 'block';
+    output.style.display = 'block';
+
+    // Construct a prompt that includes user input
+    const prompt = `Generate a 🌍 sustainable travel itinerary for a ${days}-day trip to ${location} with a budget of $${budget}.`;
+
+    axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
         prompt: prompt,
-        max_tokens: 1000,   }, {
+        max_tokens: 1000, // Adjust the max_tokens as needed
+    }, {
         headers: {
             'Authorization': `Bearer ${apiKey}`,
             'Content-Type': 'application/json',
@@ -20,11 +28,33 @@ function generateItinerary() {
     })
     .then((response) => {
         const itinerary = response.data.choices[0].text;
-        outputElement.innerText = itinerary;
+        const formattedItinerary = formatItinerary(itinerary);
+        outputElement.innerHTML = formattedItinerary;
     })
     .catch((error) => {
         console.error(error);
+    })
+    .finally(() => {
+        // Hide the loading skeleton when the response is received
+        loadingSkeleton.style.display = 'none';
     });
 
-    // In the actual code, you should handle the API response and display the itinerary.
+}
+
+function formatItinerary(itinerary) {
+    // You can add your own formatting logic here.
+    // For example, you can add line breaks and structure the content.
+    // This is a simple example, and you can customize it further.
+
+    const paragraphs = itinerary.split('\n');
+    const formattedItinerary = paragraphs
+        .filter(paragraph => paragraph.trim() !== '')
+        .map(paragraph => {
+            // Identify and bold day numbers (e.g., "Day 1", "Day 2")
+            const formattedParagraph = paragraph.replace(/(Day \d+)/g, '<strong>$1</strong>');
+            return `<p>${formattedParagraph}</p>`;
+        })
+        .join('');
+
+    return formattedItinerary;
 }
