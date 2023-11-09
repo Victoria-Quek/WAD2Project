d1ff179f190632bfb8c27d2083480feb3ba3ceec
Gareth0406 <gareth.goh.2022@scis.smu.edu.sg>
2023-11-09 20:25:51 +0800

    Gareth's File

--- Gareth/script.js
+++ Gareth/script.js
@@ -6,23 +6,35 @@
 //   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  
+  // original database
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyAl2lzXdCh1LjTPj1C5Oy0Lm85gE7usmIw",
+  //   authDomain: "wad2-17f09.firebaseapp.com",
+  //   databaseURL: "https://wad2-17f09-default-rtdb.firebaseio.com",
+  //   projectId: "wad2-17f09",
+  //   storageBucket: "gs://wad2-17f09.appspot.com",
+  //   messagingSenderId: "62610007667",
+  //   appId: "1:62610007667:web:591f8951bbe984cdacac06",
+  //   measurementId: "G-NH6H2KFT09"
+  // };
+
+  // backup database this has the like and comments
   const firebaseConfig = {
-    apiKey: "AIzaSyAl2lzXdCh1LjTPj1C5Oy0Lm85gE7usmIw",
-    authDomain: "wad2-17f09.firebaseapp.com",
-    databaseURL: "https://wad2-17f09-default-rtdb.firebaseio.com",
-    projectId: "wad2-17f09",
-    storageBucket: "gs://wad2-17f09.appspot.com",
-    messagingSenderId: "62610007667",
-    appId: "1:62610007667:web:591f8951bbe984cdacac06",
-    measurementId: "G-NH6H2KFT09"
+    apiKey: "AIzaSyDEk77OIXpJdLHLUCA7aDSsildfVsmeDCg",
+    authDomain: "wad2-8e4a4.firebaseapp.com",
+    projectId: "wad2-8e4a4",
+    storageBucket: "wad2-8e4a4.appspot.com",
+    messagingSenderId: "384425470357",
+    appId: "1:384425470357:web:970a230af00d81207cee9c",
+    measurementId: "G-TDCJHZ0MK2"
   };
 
   // Initialize Firebase
   console.log("Firebase initialising...");
   const app = firebase.initializeApp(firebaseConfig);
   const storage = getStorage(app);
+  
   
 
     // Initialize database
